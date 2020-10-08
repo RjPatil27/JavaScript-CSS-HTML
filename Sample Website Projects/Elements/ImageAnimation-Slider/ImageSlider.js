6dc0dd18ec76c71b2a63399f4d32d5c9599c4ce5
Rajat Patil <30958676+RjPatil27@users.noreply.github.com>
2020-10-08 23:27:40 +0530

    Create ImageSlider.js

---
+++ Sample Website Projects/Elements/ImageAnimation-Slider/ImageSlider.js
@@ -0,0 +1,8 @@
+var container = document.getElementById('container');
+container.onclick = function(){
+	if (container.className.includes("slide")) {
+		container.className = "container";
+	}else {
+		container.className += "slide";
+	}
+}
